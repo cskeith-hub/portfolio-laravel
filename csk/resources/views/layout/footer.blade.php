8318b4b5fdc7918a2b01fcedc50850c23a74f7a1
Colin Renkema <colin.sk.renkema@gmail.com>
2022-06-13 22:33:45 +0200

    Add footer

---
+++ csk/resources/views/layout/footer.blade.php
@@ -0,0 +1,18 @@
+<div class="container mx-auto flex flex-col mt-40"> {{-- [Margin-top on footer] --}}
+    <div class="self-center mt-1"><a href="/"><img src="/img/black.png" width="60" height="60" alt="Brand"></a></div>
+    <div class="self-center mt-1"><a href="">Home</a> | <a href="">Projects</a> | <a href="">About</a> | <a href="">Contact</a></div>
+    <div class="self-center mt-1">
+        <p> -Gain some knowledge every day- </p>
+    </div>
+    <div class="flex flex-row self-center mt-3">
+        <a href="https://becode.org/" target="blank"><img src="/img/logo-becode.png" width="50" height="50"
+            alt="BEcode"></a>
+        <a href="https://github.com/cskeith-hub" target="blank"><img
+            src="/img/7857148_github_programming_code_hub_git_icon (1).png" width="50" height="50"
+            alt="home"></a>
+        <a href="https://www.linkedin.com/in/colin-renkema-109559228/" target="blank"><img
+            src="/img/1727490_linkedin_social media_job_network_icon.png" width="50" height="50"
+            alt="home"></a>
+    </div>
+    <div class="self-center mt-3">© 2022 Colin Renkema.</div>
+</div>
